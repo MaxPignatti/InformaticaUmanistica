6b678ab89ac8ac187bae90d42efdc880ea63e499
MaxPignatti <maxpignatti@gmail.com>
2024-03-21 20:59:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, send_from_directory, jsonify, request
-from llama2 import Llama2
+#from llama2 import Llama2
 import os
 
 app = Flask(__name__, static_folder='/frontend/build', static_url_path='')
-llama = Llama2()
 
 # Serve the index.html file for the root URL
 @app.route('/')
@@ -19,7 +18,7 @@ def static_proxy(path):
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    return jsonify({"HELLO!"})
+    return jsonify({"message": "HELLO!"})
 
 # Route for handling API requests via POST
 @app.route('/api/sendText', methods=['POST'])
@@ -31,11 +30,11 @@ def handle_post():
     text = data.get('text')
 
     # Pass the text to Llama2 for processing
-    response = llama.process(text)
+    #response = llama.process(text)
 
     # Return the response obtained from Llama2
-    return jsonify({"response": response})
+    #return jsonify({"response": response})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
