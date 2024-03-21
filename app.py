00b5b31d9dfb67371a0b045e790ca271c00ad180
MaxPignatti <90690709+MaxPignatti@users.noreply.github.com>
2024-03-21 12:02:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ async def hello(request: Request):
 # Esegui l'applicazione FastAPI
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8084)
+    uvicorn.run(app)
